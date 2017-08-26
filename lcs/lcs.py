e98ac3597e2016c75d221e658795ff7dc7b218f7
akhilkoredla <akhilkoredla@gmail.com>
2017-08-26 13:39:19 +0530

    lcs code added

--- lcs/lcs.py
+++ lcs/lcs.py
@@ -1,53 +1,63 @@
-import os
-print(list[0])
+import os.path
+class Lcs(object):
+	def common_length(self,l1,l2):
+		c=0
+		for i in range(len(l1)):
+			for j in range(len(l2)):
+				l=[]
+				while i<len(l1) and j<len(l2) and l1[i]==l2[j]:
+					l.append(l1[i])
+					i=i+1
+					j=j+1
+				if len(l)!=0:
+					l="".join(l).strip()
+					if len(l)>c:
+						c=len(l)
+		return c
 
-# f1="my name is "
-# f2="name is "
+	def lcs(self,file1,file2):
+		l1=file1.split(" ")
+		len1=0
+		for i in l1:
+			len1+=len(i)
 
-l1=f1.split(" ")
-len1=0
-for i in l1:
-	len1+=len(i)
+		l2=file2.split(" ")
+		len2=0
+		for j in l2:
+			len2+=len(j)
 
-l2=f2.split(" ")
-len2=0
-for j in l2:
-	len2+=len(j)
+		a=lc.common_length(l1,l2)
+		z=a*200
+		similarity=(float(z)/(len1+len2))
+		similarity=round(similarity,2)
+		if file1==file2:
+			similarity=0.0
+		return str(similarity)
+		
 
 
-c=0
-for i in range(len(l1)):
-	for j in range(len(l2)):
-		l=[]
-		while i<len(l1) and j<len(l2) and l1[i]==l2[j]:
-			l.append(l1[i])
-			i=i+1
-			j=j+1
-		if len(l)!=0:
-			l="".join(l).strip()
-			if len(l)>c:
-				c=len(l)
-
-print c,"is length of lcs"
-z=c*200
-similarity=(float(z)/(len1+len2))
-print similarity,"% match found"
-
-path="C:/Users/Akhil/Desktop/20176075_CSPP1_PROJECT/bagofwords/bag"
+lc=Lcs()
+path="C:/Users/Akhil/Desktop/20176075_CSPP1_PROJECT/lcs/lcstxt"
 list=os.listdir(path)
 os.chdir(path)
-l=['     ']
-for i in range(len(list)):
-	l.append(list[i])
-print l
-for i in range(len(list)):
-	l1=[]
-	for j in range(0,len(list)):		
-		if i==j:
-			l1.append(0.0)
-		else:
-			file1=open(list[i],"r").read().lower()
-			file2=open(list[j],"r").read().lower()
-			l2=lsc(file1,file2)
-			l1.append(l2)
-	print list[i],l1		
+matrix=[]
+submatrix=[]
+submatrix.append('      ')
+submatrix.extend(list)
+matrix.append(submatrix)
+for i in list:
+	submatrix=[]
+	submatrix.append(i)
+	for j in list:
+		file1=open(i,'r').read().lower()
+		file2=open(j,'r').read().lower()
+		x=lc.lcs(file1,file2)
+		submatrix.append(x)
+	matrix.append(submatrix)
+for i in range(len(matrix)):
+	if i==0:
+		k='     '.join(matrix[i])
+		print(k)
+	else:
+		k='        '.join(matrix[i])
+		print(k)
